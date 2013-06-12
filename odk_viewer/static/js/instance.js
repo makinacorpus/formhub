7635bb9babde17677b2324808aeddfe189aa208d
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2013-06-12 21:55:26 +0200

    feat(data_view): display groups and repeated data

--- odk_viewer/static/js/instance.js
+++ odk_viewer/static/js/instance.js
@@ -1,8 +1,9 @@
-Question = function(questionData)
+Question = function(questionData, parent)
 {
     this.name = questionData.name;
     this.type = questionData.type;
     this.label = questionData.label;
+    this.parent = parent;
 }
 
 Question.prototype.getLabel = function(language)
@@ -30,23 +31,41 @@ Question.prototype.getLabel = function(language)
     return this.name;
 }
 
-function parseQuestions(children, prefix, cleanReplacement)
+function parseQuestions(children, prefix, cleanReplacement, tree)
 {
     var idx;
     cleanReplacement = typeof cleanReplacement !== 'undefined' ? cleanReplacement : '_';
-
+    if (tree===undefined) {
+        tree = questionsTree;
+    };
     for(idx in children)
     {
         var question = children[idx];
+        var qid = ((prefix?prefix:'') + question.name)
+        var oquestion = new Question(question, tree['question'])
+        var subtree = {
+            "question": oquestion,
+            "jsonobj": question,
+            "questions": []};
         //@TODO: do we just want to add anything with children, concern could be it item has children and is alos avalid question - if thats possible
         if(question.hasOwnProperty('children') && (question.type == "group" || question.type == "note" || question.type == "repeat"))
         {
-            parseQuestions(question.children, ((prefix?prefix:'') + question.name + cleanReplacement));
+            parseQuestions(question.children,
+                qid + cleanReplacement,
+                undefined, subtree);
+
         }
-        else
-        {
-            // TODO: question class that has accessor mesthods for type, label, language etc
-            questions[((prefix?prefix:'') + question.name)] = new Question(question);
+        questions[qid] = oquestion;
+        // only nest repeats
+        tree['questions'].push(subtree);
+        if(question.type=="group" || question.type=="note"){
+            $.each(subtree['questions'], function(i, itm){
+                tree['questions'].push(itm);
+            }
+            );
+            subtree['questions'] = [];
         }
     }
 }
+
+
